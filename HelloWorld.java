7a878e724c80d2de96ae21fa1c4e97ee9a6a3ef1
Bright Gee Varghese <brightlsom@gmail.com>
2025-01-31 21:22:47 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("second line in our file");
-        System.out.println("An another person added 3rd line");
     }
 }
