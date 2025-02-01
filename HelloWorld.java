806ab91110420b71bfd6513b4ada524afbcbf19d
Bright Gee Varghese <bvarghese@miu.edu>
2025-01-31 21:29:04 -0600

    Added a for loop to print i

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("second line in our file");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
     }
 }
